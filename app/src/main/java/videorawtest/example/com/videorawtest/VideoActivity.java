8396a589cce452744e25c4a69070fe9d533b173a
steven18 <2602031200@qq.com>
2018-04-27 17:25:08 +0800

    3

---
+++ app/src/main/java/videorawtest/example/com/videorawtest/VideoActivity.java
@@ -0,0 +1,22 @@
+package videorawtest.example.com.videorawtest;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"//"+ R.raw.test));
+        videoView.start();
+    }
+}
