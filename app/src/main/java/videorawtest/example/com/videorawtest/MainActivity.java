6f45205f435fc6556da24a11eafc706a794bbca5
steven18 <2602031200@qq.com>
2018-04-30 17:23:07 +0800

    add fullscreen showing image

--- app/src/main/java/videorawtest/example/com/videorawtest/MainActivity.java
+++ app/src/main/java/videorawtest/example/com/videorawtest/MainActivity.java
@@ -1,19 +1,24 @@
 package videorawtest.example.com.videorawtest;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,23 +26,51 @@ import androidlibzxing.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final static int SCANNIN_GREQUEST_CODE = 1;
-    /**
-     * 显示扫描结果
-     */
+
     private TextView mTextView;
-    /**
-     * 显示扫描拍的图片
-     */
     private ImageView mImageView;
 
+    private Boolean bTouchImage = false;
+    private Bitmap mBitmap;
+
+    private float mScaleWidth;
+    private float mScaleHeight;
+
+    private float mScreenWidth;
+    private float mScreenHeight;
+
+
+    //动态的ImageView
+    private ImageView getImageView() {
+        ImageView iv = new ImageView(this);
+        iv.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        Matrix matrix = new Matrix();
+        float nScale = (mScaleWidth > mScaleHeight) ? mScaleHeight : mScaleWidth;
+        matrix.postScale(nScale, nScale);
+        Bitmap newBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
+
+        iv.setImageBitmap(newBitmap);
+
+        return iv;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBitmap = null;
 
-        Button btn = (Button) findViewById(R.id.btnPlayVideo);
-        btn.setOnClickListener(new View.OnClickListener() {
+        DisplayMetrics dm = new DisplayMetrics();//创建矩阵
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        mScreenWidth = dm.widthPixels; //得到屏幕的宽度
+        mScreenHeight = dm.heightPixels; //得到屏幕的高度
+
+        bTouchImage = false;
+
+        Button btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        btnPlayVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, VideoActivity.class);
@@ -52,22 +85,47 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "action",
                     Toast.LENGTH_SHORT).show();
         }
-        mTextView = (TextView) findViewById(R.id.result);
+
         mImageView = (ImageView) findViewById(R.id.qrcode_bitmap);
+        mTextView = (TextView) findViewById(R.id.qrcode_title);
 
         mImageView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-
                 Bitmap bitmap = ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
                 mTextView.setText(ZxingUtils.DecodeBitmap(bitmap));
-                return false;
+                return true;// 必须返回true,防止事件继续往下派发，比如响应点击事件
             }
         });
 
-        //点击按钮跳转到二维码扫描界面，这里用的是startActivityForResult跳转
-        //扫描完了之后调到该界面
-        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+        mImageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBitmap == null)
+                    return;
+                final Dialog dialog = new Dialog(MainActivity.this,R.style.edit_AlertDialog_style);
+                ImageView newImageView = getImageView();
+                dialog.setContentView(newImageView);
+                MainActivity.this.setVisible(false);
+                newImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        MainActivity.this.setVisible(true);
+                    }
+                });
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        dialog.dismiss();
+                        MainActivity.this.setVisible(true);
+                    }
+                });
+                dialog.show();
+            }
+        });
+        Button btnScan = (Button) findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -77,8 +135,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
             }
         });
-        //生成二维码
-        findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
+
+        Button btnGenerate = (Button) findViewById(R.id.btnGenerate);
+        btnGenerate.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -88,8 +147,20 @@ public class MainActivity extends AppCompatActivity {
                         .setView(input)
                         .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog01, int whichButton01) {
+
                                 String value = input.getText().toString();
-                                mImageView.setImageBitmap(ZxingUtils.createBitmap(value, 200, 200));
+                                mBitmap = ZxingUtils.createBitmap(value, 400, 400);
+                                mImageView.setImageBitmap(mBitmap);
+
+                                int bitmapWidth = mBitmap.getWidth();
+                                int bitmapHeight = mBitmap.getHeight();
+                                DisplayMetrics dm = new DisplayMetrics();//创建矩阵
+                                getWindowManager().getDefaultDisplay().getMetrics(dm);
+                                mScreenWidth = dm.widthPixels; //得到屏幕的宽度
+                                mScreenHeight = dm.heightPixels; //得到屏幕的高度
+                                mScaleWidth = ((float) mScreenWidth) / bitmapWidth;
+                                mScaleHeight = ((float) mScreenHeight) / bitmapHeight;
+
                             }
                         }).setCancelable(true).show();
             }
@@ -104,9 +175,7 @@ public class MainActivity extends AppCompatActivity {
             case SCANNIN_GREQUEST_CODE:
                 if (resultCode == RESULT_OK) {
                     Bundle bundle = data.getExtras();
-                    //显示扫描到的内容
                     mTextView.setText(bundle.getString("result"));
-                    //显示
                     mImageView.setImageBitmap((Bitmap) data.getParcelableExtra("bitmap"));
                 }
                 break;
